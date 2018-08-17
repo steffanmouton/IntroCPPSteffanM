b2320cf51ae1c7b5439a6ba2643921065fdce3e0
Steffan Mouton <smouton8993@gmail.com>
2018-08-17 11:10:37 -0500

    Completed the rest of assignment

--- Variables/Source.cpp
+++ Variables/Source.cpp
@@ -2,41 +2,47 @@
 
 int main()
 {
-	int number;
-	number = 23;
-	std::cout << number << std::endl;
-	number = 30;
-	std::cout << number << std::endl;
-	number = -12;
-	std::cout << number << std::endl;
-	number++;
-	std::cout << number << std::endl;
-	number = 10;
-	std::cout << number << std::endl;
-	std::cout << std::endl;
+	{
+		int number;
+		number = 23;
+		std::cout << number << std::endl;
+		number = 30;
+		std::cout << number << std::endl;
+		number = -12;
+		std::cout << number << std::endl;
+		number++;
+		std::cout << number << std::endl;
+		number = 10;
+		std::cout << number << std::endl;
+		std::cout << std::endl;
+	}
 
+	{
+		int numA;
+		int numB;
+		int sum;
+		numA = 20;
+		numB = 30;
+		sum = numA + numB;
+		std::cout << sum << std::endl;
+		std::cout << std::endl;
+	}
 
-	int numA;
-	int numB;
-	int sum;
-	numA = 20;
-	numB = 30;
-	sum = numA + numB;
-	std::cout << sum << std::endl;
-	std::cout << std::endl;
-
-	float numC, numD, sumCD;
-	numC = 13.12f;
-	numD = 27.52f;
-	sumCD = numC + numD;
-	std::cout << sumCD << std::endl;
-	std::cout << std::endl;
-
-	int userInput;
-	std::cout << "Input a whole number: " << std::endl;
-	std::cin >> userInput;
-	std::cout << "You entered: " << userInput << std::endl;
-
+	{
+		float numC, numD, sumCD;
+		numC = 13.12f;
+		numD = 27.52f;
+		sumCD = numC + numD;
+		std::cout << sumCD << std::endl;
+		std::cout << std::endl;
+	}
+	
+	{
+		int userInput;
+		std::cout << "Input a whole number: " << std::endl;
+		std::cin >> userInput;
+		std::cout << "You entered: " << userInput << std::endl;
+	}
 	//Exercise Guesses: A) 14
 	{
 		int numberA = 5;
@@ -83,7 +89,8 @@ int main()
 	{
 		float y = 13;
 		y /= 2;
-		std::cout << "F)" << y << std::endl;		// Answer is 6.5, thought 
+		std::cout << "F)" << y << std::endl;
+		// Answer is 6.5, thought 
 	}
 
 	// Celsius to Fahrenheit)
@@ -102,7 +109,8 @@ int main()
 		std::cout << "Celsius to Fahrenheit)" << std::endl;
 		std::cout << "Celsius: " << degCelsius << std::endl;
 		std::cout << "Fahrenheit: " << degFahrenheit << std::endl;
-	}	// ...
+	}
+
 	// Area of a Rectangle)
 	{
 		float rectWidth = 0.0f; // Modify this variable below.
@@ -117,7 +125,8 @@ int main()
 
 		std::cout << "Area of a Rectangle)" << std::endl;
 		std::cout << "H: " << rectHeight << " , W: " << rectWidth << std::endl;
-		std::cout << "Area: " << rectArea << std::endl;	}
+		std::cout << "Area: " << rectArea << std::endl;
+	}
 
 	// Average of Five)
 	{
@@ -139,19 +148,77 @@ int main()
 
 		std::cout << "Average of Five)" << std::endl;
 		std::cout << a << "," << b << "," << c << "," << d << "," << e << std::endl;
-		std::cout << "avg: " << avg << std::endl;	}	// Number Swap)
+		std::cout << "avg: " << avg << std::endl;
+	}
+
+
+	// Number Swap)
 	{
 		int x = 13;
 		int y = 24;
 		// <Your work goes here>
-		x = x - x + y;
-		y = y - y + x;
+		std::cout << "Input a Value for X\n" << std::endl;
+		std::cin >> x;
+		std::cout << "For X you chose " << x << std::endl;
+		std::cout << "Input a Value for Y\n" << std::endl;
+		std::cin >> y;
+		std::cout << "For Y you chose " << y << std::endl;
+
+
+		x = x + y; 
+		y = x - y; 
+		x = x - y; 
+
 		std::cout << "Number Swap)" << std::endl;
 		std::cout << "x is " << x << std::endl;
-		std::cout << "y is " << y << std::endl;	}
+		std::cout << "y is " << y << std::endl;
+	}
 
+	// Fun Facts for Your Age)
+	{
+		int age = 0;
+		int ageMonths = age * 12;
+		float ageDecades = age / 10;
+		int lifeJapan = 84 - age;
+		int lifeUS = 79 - age;
+		int lifeSierra = 50 - age;
 
+		std::cout << "Howdy! You are " << age << " years old!\n";
+		std::cout << "That's at least " << ageMonths << " months!\n";
+		std::cout << "And " << ageDecades << " decades!\n";
+		std::cout << "If you live in Japan, you can expect to live " << lifeJapan << " more years.";
+		std::cout << "If you live in The United States, you can expect to live " << lifeUS << " more years.";
+		std::cout << "If you live in Sierra Leone, you can expect to live " << lifeSierra << " more years.";
+	}
 
+	// Conforming to Conventions)
+	{
+		//From camel case to lower snake:
+		int age;
+		int bullet_count;
+		float dog_years;
+		int qty;
+		float shield_value;
+		float defense_matrix_cooldown;
+		int red_armor_value;
+		float red_armor_ratio;
+		int happiness;
+		int gandhi_aggression;
+	}
+
+	// The Right Tool for the Job)
+	{
+		int num; // integer
+		float num2; // single precision floating point
+		long num3;
+		short num4;
+		bool lightsOn;
+		double num5;
+		unsigned int num6;
+		signed int num7;
+		char a;
+		long double ten;
+	}
 
 	system("pause");
 }
