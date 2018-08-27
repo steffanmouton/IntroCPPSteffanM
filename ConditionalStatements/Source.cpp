ea6c0a3c82623199442412cf9bda2864b44ce9cf
steffanmouton <36455372+steffanmouton@users.noreply.github.com>
2018-08-27 07:01:33 -0500

    Homework done
    homework done

--- ConditionalStatements/Source.cpp
+++ ConditionalStatements/Source.cpp
@@ -4,112 +4,112 @@ int main()
 {
 
 
-	//Grading with If and Else If
-	{
-		int grade;
-		char gradeLetter;
+	////Grading with If and Else If
+	//{
+	//	int grade;
+	//	char gradeLetter;
 
-		std::cout << "Input a grade value:\n";
-		std::cin >> grade;
+	//	std::cout << "Input a grade value:\n";
+	//	std::cin >> grade;
 
-		if (grade >= 90)
-			gradeLetter = 'A';
-		else if (grade >= 80 && grade < 90)
-			gradeLetter = 'B';
-		else if (grade >= 70 && grade < 80)
-			gradeLetter = 'C';
-		else if (grade >= 60 && grade < 70)
-			gradeLetter = 'D';
-		else
-			gradeLetter = 'F';
+	//	if (grade >= 90)
+	//		gradeLetter = 'A';
+	//	else if (grade >= 80 && grade < 90)
+	//		gradeLetter = 'B';
+	//	else if (grade >= 70 && grade < 80)
+	//		gradeLetter = 'C';
+	//	else if (grade >= 60 && grade < 70)
+	//		gradeLetter = 'D';
+	//	else
+	//		gradeLetter = 'F';
 
-		std::cout << gradeLetter << std::endl;
+	//	std::cout << gradeLetter << std::endl;
 
-	}
+	//}
 
-	//FizzBuzz Multiples
-	{
-		int input = 0;
-		std::cout << "Fizzbuzz -- Enter a number:\n";
-		std::cin >> input;
+	////FizzBuzz Multiples
+	//{
+	//	int input = 0;
+	//	std::cout << "Fizzbuzz -- Enter a number:\n";
+	//	std::cin >> input;
 
-		if (input % 3 == 0 && input % 5 == 0)
-		{
-			std::cout << "FizzBuzz\n";
-		}
-		else if (input % 3 == 0)
-		{
-			std::cout << "Fizz\n";
-		}
-		else if (input % 5 == 0)
-		{
-			std::cout << "Buzz\n";
-		}
+	//	if (input % 3 == 0 && input % 5 == 0)
+	//	{
+	//		std::cout << "FizzBuzz\n";
+	//	}
+	//	else if (input % 3 == 0)
+	//	{
+	//		std::cout << "Fizz\n";
+	//	}
+	//	else if (input % 5 == 0)
+	//	{
+	//		std::cout << "Buzz\n";
+	//	}
 
-	}
+	//}
 
-	//Simple Calculator
-	{
-		float num1, num2;
-		char op;
+	////Simple Calculator
+	//{
+	//	float num1, num2;
+	//	char op;
 
-		std::cout << "Enter two numbers: \n";
-		std::cin >> num1 >> num2;
-		std::cout << "Do you want to Add(+) or Subtract(-)?\n";
-		std::cin >> op;
+	//	std::cout << "Enter two numbers: \n";
+	//	std::cin >> num1 >> num2;
+	//	std::cout << "Do you want to Add(+) or Subtract(-)?\n";
+	//	std::cin >> op;
 
-		switch (op)
-		{
-		case '+':
-			std::cout << num1 + num2 << std::endl;
-			break;
-		case '-':
-			std::cout << num1 - num2 << std::endl;
-			break;
-		default:
-			std::cout << "Invalid input, need + or -";
-			break;
-		}
+	//	switch (op)
+	//	{
+	//	case '+':
+	//		std::cout << num1 + num2 << std::endl;
+	//		break;
+	//	case '-':
+	//		std::cout << num1 - num2 << std::endl;
+	//		break;
+	//	default:
+	//		std::cout << "Invalid input, need + or -";
+	//		break;
+	//	}
 
-	}
+	//}
 
-	//Is the Player Alive?
-	{
-		int hp;
+	////Is the Player Alive?
+	//{
+	//	int hp;
 
-		std::cout << "Input current hp value:\n";
-		std::cin >> hp;
+	//	std::cout << "Input current hp value:\n";
+	//	std::cin >> hp;
 
-		if (hp > 0)
-		{
-			std::cout << "You are alive.\n";
+	//	if (hp > 0)
+	//	{
+	//		std::cout << "You are alive.\n";
 
-			if (hp < 25 && hp > 0)
-			{
-				std::cout << "You are in critical condition!\n";
-			}
+	//		if (hp < 25 && hp > 0)
+	//		{
+	//			std::cout << "You are in critical condition!\n";
+	//		}
 
-			std::cout << "You have " << hp << " health remaining.\n";
-		}
-		else
-		{
-			std::cout << "You are dead.\n";
-		}
+	//		std::cout << "You have " << hp << " health remaining.\n";
+	//	}
+	//	else
+	//	{
+	//		std::cout << "You are dead.\n";
+	//	}
 
-	}
+	//}
 
 	// HOMEWORK 22AUGUST
 
 	// 1 
-	/*{
+	{
 		int x, y;
-		std::cin >> x, y;
+		std::cin >> x >> y;
 
 		if (y == 0)
 		{
 			x = 100;
 		}
-	}*/
+	}
 
 	// 2
 	{
@@ -130,12 +130,33 @@ int main()
 
 	// 3
 	{
+		int a, b, c, d, e;
+		
+		std::cout << "Please input 5 numbers:\n";
+		std::cin >> a >> b >> c >> d >> e;
+		
+		if (a + b + c + d + e == 0)
+		{
+			std::cout << a << " " << b << " " << c << " " << d << " " << e;
+		}
 
+		if (a + b + c + d + e > 0)
+		{
+			if (a > b && a > c && a > d && a > e && b > c && b > d && b > e && c > d && c > e && d > e)
+			{
+				std::cout << a << " " << b << " " << c << " " << d << " " << e;
+			}
+
+			if (b > a && b > c && b > d && b > e && a > c && a > d && a > e && c > d && c > e && d > e)
+			{
+				std::cout << b << " " << a << " " << c << " " << d << " " << e;
+			}
+		}
 	}
 
 	// 4 Convert to switch
 	{
-		int choice;
+		/*int choice;
 		std::cin >> choice;
 
 		switch (choice)
@@ -167,20 +188,48 @@ int main()
 			break;
 		}
 
-		}
+		}*/
 	}
 
 	// 5 Convert to Ternary
-	{
+	/*{
 		int x, y;
 		std::cout << "Enter two numbers: \n";
 		std::cin >> x >> y;
 
 		y = (x == 0) ? 0 : (10 / x);
-	}
+	}*/
 
 	// 6 Calculator
 
+	{
+		float num1, num2;
+		char op;
+
+		std::cout << "Enter two numbers: \n";
+		std::cin >> num1 >> num2;
+		std::cout << "Do you want to Add(+), Subtract(-), Divide (/) or Multiply(*)?\n";
+		std::cin >> op;
+
+		switch (op)
+		{
+		case '+':
+			std::cout << num1 + num2 << std::endl;
+			break;
+		case '-':
+			std::cout << num1 - num2 << std::endl;
+			break;
+		case '/':
+			std::cout << num1 / num2 << std::endl;
+			break;
+		case '*':
+			std::cout << num1 * num2 << std::endl;
+			break;
+		default:
+			std::cout << "Invalid input, need +  , - , / , * \n";
+			break;
+		}
+	}
 	// 7 Month
 	{
 		int month;
@@ -275,6 +324,9 @@ int main()
 	
 	a) True  b) True  c) False  d) True  e) False
 	*/
+
+
+	//Problem from in class, Count to Ten
 /*
 	{
 	
@@ -289,7 +341,7 @@ int main()
 
 
 
-
+	system("pause");
 
 
 }
--- Loops/Source.cpp
+++ Loops/Source.cpp
@@ -69,13 +69,212 @@ int main()
 
 	// Loops Hwk
 	// Exercise 1
+
+
 	{
 		int i;
 		for (i = 100; i > 0; i--)
 		{
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
 
+	{
+		int i;
+		for (i = 100; i >= 0; i--)
+		{
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	{
+		int i;
+		for (i = 99; i >= 0; i--)
+		{
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	{
+		int i;
+		for (i = 100; i > 0; i--)
+		{
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	{
+		int i;
+		for (i = 100; i >= 0; i-=2)
+		{
+			std::cout << i << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	// Exercise 2
+
+	{
+		int i = 0;
+		for (i = 0; i <= 100; i++)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				std::cout << "FizzBuzz ";
+			}
+			else if (i % 3 == 0 )
+			{
+				std::cout << "Fizz ";
+			}
+			else if (i % 5 == 0)
+			{
+				std::cout << "Buzz ";
+			}
+			else
+			{
+				std::cout << i << " ";
+			}
+		}
+
+		std::cout << std::endl;
+	}
+
+
+	// Exercise 3
+	{
+		int i = 0;
+		int sum = 0;
+		for (i = 0; i <= 1000; i++)
+		{
+			if (i % 3 == 0 || i % 5 == 0)
+			{
+				sum = sum + i;
+			}
+		}
+		std::cout << "The sum of all multiples of 3 and 5 from 0 to 1000 is " << sum << ". \n";
+	}
+
+	//Exercise 4
+	{
+		int i = 100;
+		while (i >= 0)
+		{
+			std::cout << i << " ";
+			i--;
+
+
+			if (i < 0)
+			{
+				std::cout << std::endl;
+				break;
+			}
 		}
 	}
 
+	{
+		int i = 99;
+		while (i >= 0)
+		{
+			std::cout << i << " ";
+			i--;
+
+
+			if (i < 0)
+			{
+				std::cout << std::endl;
+				break;
+			}
+		}
+	}
+
+	{
+		int i = 100;
+		while (i > 0)
+		{
+			std::cout << i << " ";
+			i--;
+
+
+			if (i > 0)
+			{
+				std::cout << std::endl;
+				break;
+			}
+		}
+	}
+
+	{
+		int i = 100;
+		while (i >= 0)
+		{
+			std::cout << i << " ";
+			i-=2;
+
+
+			if (i < 0)
+			{
+				std::cout << std::endl;
+				break;
+			}
+		}
+	}
+
+	{
+		int i = 0;
+		while (i <= 100)
+		{
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				std::cout << "FizzBuzz ";
+			}
+			else if (i % 3 == 0)
+			{
+				std::cout << "Fizz ";
+			}
+			else if (i % 5 == 0)
+			{
+				std::cout << "Buzz ";
+			}
+			else
+			{
+				std::cout << i << " ";
+			}
+
+			if (i > 100)
+			{
+				std::cout << std::endl;
+				break;
+			}
+
+			i++;
+		}
+	}
+	 // I have no clue why this outputs to 0.
+	{
+		int i = 0;
+		int sum = 0;
+		while (i <= 1000)
+		{
+			i++;
+
+			if (i % 3 == 0 || i % 5 == 0)
+			{
+				sum = sum + i;
+			}
+
+			if (i = 1000)
+			{
+				std::cout << "\nThe sum of all multiples of 3 and 5 from 0 to 1000 is " << sum << ". \n";
+				break;
+			}
+
+		
+		}
+
+	}
 	system("pause");
 }
