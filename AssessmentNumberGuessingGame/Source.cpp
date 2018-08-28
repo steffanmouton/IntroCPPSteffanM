3b6d88d1b672cde92d40316abc600f52689869f1
steffanmouton <36455372+steffanmouton@users.noreply.github.com>
2018-08-27 22:02:43 -0500

    NumberGuessingGame Draft
    FirstDraft NGG complete

---
+++ AssessmentNumberGuessingGame/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	int guess = 10;
+	char start = ' ';
+
+	std::cout << "Think of a number between 1 and 20\n Type 'S' when ready to start.\n";
+	std::cin >> start;
+
+	while (start == 'S')
+	{
+		char input;
+		
+
+		std::cout << "Is your number '>', '<', or '=' to " << guess << "?\n";
+		std::cin >> input;
+
+		switch (input)
+		{
+		case '>':
+			guess+= 1;
+			break;
+		case '<':
+			guess-= 1;
+			break;
+		case '=':
+			std::cout << "The number you chose was " << guess << ".\n";
+			system("pause");
+			return 0;
+		}
+
+	}
+}
