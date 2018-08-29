adbf8e9d16d3b4d25134134ff3d9a53bfaf9f925
Steffan Mouton <smouton8993@gmail.com>
2018-08-29 18:00:22 -0500

    End of Day work

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -164,6 +164,20 @@ int main()
 		std::cout << "Largest: " << largest << std::endl;
 	}
 
+	//Homework 6
+
+	{
+		int arr[3][3];
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+
+			}
+		}
+
+	}
 
 
 	system("pause");
--- CharacterArrays/Source.cpp
+++ CharacterArrays/Source.cpp
@@ -35,59 +35,113 @@ int main()
 	//Create String Compare Function
 
 	{
-		char nameOne[] = "Steffan";
-		char nameTwo[] = "Steffan";
-		int lenOne = 0;
-		int lenTwo = 0;
+		//char nameOne[] = "Steffan";
+		//char nameTwo[] = "Steffan";
+		//int lenOne = 0;
+		//int lenTwo = 0;
 
-		//is length the same?
-		bool sameLen = true;
-		while (sameLen)
-		{
-			while (nameOne[lenOne] != '\0')
-			{
-				lenOne++;
-			}
-			while (nameTwo[lenTwo] != '\0')
-			{
-				lenTwo++;
-			}
+		////is length the same?
+		//bool sameLen = true;
+		//while (sameLen)
+		//{
+		//	while (nameOne[lenOne] != '\0')
+		//	{
+		//		lenOne++;
+		//	}
+		//	while (nameTwo[lenTwo] != '\0')
+		//	{
+		//		lenTwo++;
+		//	}
 
-			if (lenOne != lenTwo)
-			{
-				std::cout << "String length is not the same.\n";
-				sameLen = false;
-			}
-			else
-			{
-				//are values the same?
-				int strCmp = 0;
-				for (int i = 0; i < lenOne; i++)
+		//	if (lenOne != lenTwo)
+		//	{
+		//		std::cout << "String length is not the same.\n";
+		//		sameLen = false;
+		//	}
+		//	else
+		//	{
+		//		//are values the same?
+		//		int strCmp = 0;
+		//		for (int i = 0; i < lenOne; i++)
 
-				{
-					if (nameOne[i] == nameTwo[i])
-					{
-						strCmp = 0;
-						continue;
-					
-					}
-					else if (nameOne[i] < nameTwo[i])
-					{
-						strCmp = -1;
-						break;
-					
-					}
-					else
-					{
-						strCmp = 1;
-						break;
-						
-					}
-				} 
-				std::cout << strCmp << std::endl;
-				sameLen = false;
-			}
-		}
-		system("pause");
+		//		{
+		//			if (nameOne[i] == nameTwo[i])
+		//			{
+		//				strCmp = 0;
+		//				continue;
+		//			
+		//			}
+		//			else if (nameOne[i] < nameTwo[i])
+		//			{
+		//				strCmp = -1;
+		//				break;
+		//			
+		//			}
+		//			else
+		//			{
+		//				strCmp = 1;
+		//				break;
+		//				
+		//			}
+		//		} 
+		//		std::cout << strCmp << std::endl;
+		//		sameLen = false;
+		//	}
+		//}
+		//system("pause");
 	}
+
+	// Exercises
+
+	//1
+	{
+		char name[255];
+
+		std::cout << "Please input your First and Last Name\n" << std::endl;
+		std::cin.getline(name, 255);
+
+		std::cout << name << std::endl;
+
+	}
+
+	//2
+	{
+		char name[255];
+		int nameLen = 0;
+		int i = 0;
+
+		std::cout << "Please input your First and Last Name\n" << std::endl;
+		std::cin.getline(name, 255);
+
+		while (name[nameLen] != '\0')
+		{ 
+			nameLen++;
+		}
+
+		for (i = nameLen -1; i >= 0; i--)
+		{
+			std::cout << name[i];
+		}
+		std::cout << std::endl;
+	}
+
+	//3
+	{
+		char names[5][255];
+		int nameLen = 0;
+		int i = 0;
+	}
+
+	//4 - '\a'
+
+	//5
+	{
+
+	}
+
+	//6
+	{
+
+	}
+	system("pause");
 }
