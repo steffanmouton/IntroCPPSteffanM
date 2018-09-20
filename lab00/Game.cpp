36343894e6081a8d66530f606865bfb60728aed4
steffanmouton <36455372+steffanmouton@users.noreply.github.com>
2018-09-20 12:31:09 -0500

    Update Game.cpp

--- lab00/Game.cpp
+++ lab00/Game.cpp
@@ -1,9 +1,18 @@
 #include "Game.h"
 
+//each hero deals damage to the other, whoever is left 
+//with more health is returned as winner
 Hero Game::Battle(Hero & h1, Hero & h2)
 {
 	h1.Fight(h2);
 	h2.Fight(h1);
 
-	if (h1.mHealth > )
+	if (h1.mHealth > h2.mHealth)
+	{
+		return h1;
+	}
+	else
+	{
+		return h2;
+	}
 }
