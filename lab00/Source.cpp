0fff0733121ac99429cbace83722ad5fa4212562
steffanmouton <36455372+steffanmouton@users.noreply.github.com>
2018-09-26 08:28:14 -0500

    StackStuck

--- lab00/Source.cpp
+++ lab00/Source.cpp
@@ -10,7 +10,20 @@ int main()
 	Hero hero1 = Hero("IronMan");
 	Hero hero2 = Hero("CaptainAmerica");
 
+	Hero* IronMan = &hero1;
+	Hero* Cap = &hero2;
+
 	Game Arena;
+
+	Stack Team1;
+	Stack Team2;
+	Stack Winners;
+	Stack Losers;
+
+	Team1.Push(IronMan);
+	Team2.Push(Cap);
+
+	Arena.Battle(Team1.Top(), Team2.Top());
 
 	std::cout << "The winner is: " << Arena.Battle(hero1, hero2).Info();
 
